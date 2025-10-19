828e86fb3e3a4f4a28f686a479b3741cf3153ddf
ShunTay12 <tayshunda@gmail.com>
2025-10-19 22:06:51 +0800

    edit register_tortoise() params

--- 09 ORM/main.py
+++ 09 ORM/main.py
@@ -13,8 +13,8 @@ app.include_router(student_api, prefix="/student", tags=["course enrollment api"
 register_tortoise(
     app=app,
     config=TORTOISE_ORM,
-    # generate_schemas=True,
-    # add_exception_handlers=True,
+    # generate_schemas=True,    # if database is empty, the create the schema, DON'T use it in production
+    # add_exception_handlers=True,  # enable exception handlers for the ORM, DON'T use it in production, it will leak information
 )
 
 
